0686d6c4a6eee8f9728be050a3ee228178a9ed7e
Saeed Mahmud <119888472+xsayedmahmud@users.noreply.github.com>
2023-04-29 23:13:33 +0300

    Implement validation for names & email, fix pwd bug

--- app.js
+++ app.js
@@ -1,9 +1,112 @@
 const form = document.querySelector("form");
-const fName = document.querySelector("#fName");
-const lName = document.querySelector("#lName");
-const email = document.querySelector("#email");
 
 const phonenum = document.querySelector("#phonenum");
+
+//name validation below
+
+const fName = document.querySelector("#fName");
+const fNameLabel = document.querySelector('label[for="fName"]');
+const fNameErrorMsg = document.querySelector(".fNameErrorMsg");
+
+const lName = document.querySelector("#lName");
+const lNameLabel = document.querySelector('label[for="lName"]');
+const lNameErrorMsg = document.querySelector(".lNameErrorMsg");
+
+const nameRegex = /^[a-z ,.'-]+$/i;
+
+function validateName(nameInput, nameLabel, nameErrorMsg) {
+  const nameValue = nameInput.value;
+  const curNameLabel = nameLabel.textContent;
+
+  if (nameValue === "") {
+    nameErrorMsg.textContent = `Please enter a ${curNameLabel}`;
+    nameLabel.style.display = "none";
+  } else if (!nameRegex.test(nameValue)) {
+    nameErrorMsg.textContent = `Name cannot contain special characters except ,.'-`;
+    nameLabel.style.display = "none";
+  } else {
+    nameErrorMsg.textContent = "";
+    nameLabel.style.display = "block";
+  }
+}
+
+fName.addEventListener("input", () => {
+  validateName(fName, fNameLabel, fNameErrorMsg);
+});
+
+fName.addEventListener("blur", () => {
+  if (fName.value.length === 0) {
+    fNameErrorMsg.classList.add("unfocus");
+  }
+  validateName(fName, fNameLabel, fNameErrorMsg);
+});
+
+fName.addEventListener("focus", () => {
+  fNameLabel.classList.add("labelUp");
+  if (fNameErrorMsg.classList.contains("unfocus")) {
+    fNameErrorMsg.classList.remove("unfocus");
+  }
+});
+
+lName.addEventListener("input", () => {
+  validateName(lName, lNameLabel, lNameErrorMsg);
+});
+
+lName.addEventListener("blur", () => {
+  if (lName.value.length === 0) {
+    lNameErrorMsg.classList.add("unfocus");
+  }
+
+  validateName(lName, lNameLabel, lNameErrorMsg);
+});
+
+lName.addEventListener("focus", () => {
+  lNameLabel.classList.add("labelUp");
+  if (lNameErrorMsg.classList.contains("unfocus")) {
+    lNameErrorMsg.classList.remove("unfocus");
+  }
+});
+
+// email validation below
+
+const email = document.querySelector("#email");
+const emailLabel = document.querySelector('label[for="email"]');
+const emailErrorMsg = document.querySelector(".emailErrorMsg");
+const emailRegex =
+  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+function validateEmail(emailInput, emailLabel, emailErrorMsg) {
+  let emailValue = emailInput.value;
+
+  if (email.value === "") {
+    emailLabel.style.display = "none";
+    emailErrorMsg.textContent = "Please enter an email address";
+  } else if (!emailRegex.test(emailValue)) {
+    emailErrorMsg.textContent = "Please enter a valid email address";
+    emailLabel.style.display = "none";
+  } else {
+    emailErrorMsg.textContent = "";
+    emailLabel.style.display = "block";
+  }
+}
+
+email.addEventListener("input", () => {
+  validateEmail(email, emailLabel, emailErrorMsg);
+});
+
+email.addEventListener("blur", () => {
+  if (email.value === "") {
+    emailErrorMsg.classList.add("unfocus");
+  }
+  validateEmail(email, emailLabel, emailErrorMsg);
+});
+
+email.addEventListener("focus", () => {
+  emailLabel.classList.add("labelUp");
+  if (emailErrorMsg.classList.contains("unfocus")) {
+    emailErrorMsg.classList.remove("unfocus");
+  }
+});
 
 // password validation below
 
@@ -15,8 +118,9 @@ const aNumber = document.querySelector("#a-number");
 const repetition = document.querySelector("#repetition");
 const usingnames = document.querySelector("#usingnames");
 const pwdMeter = document.querySelector("#pwdMeter");
-const pwdLabel = document.querySelector("#pwdLabel");
+const pwdMeterLabel = document.querySelector("#pwdMeterLabel");
 const pwdErrorMsg = document.querySelector(".pwdErrorMsg");
+const pwdLabel = document.querySelector('label[for="pwd"]');
 
 const eightCharPattern = /^.{8,}$/;
 const uppercasePattern = /[A-Z]/;
@@ -37,6 +141,7 @@ function validatePassword() {
 
   if (pwdValue === "") {
     repetition.checked = false;
+    pwdLabel.style.display = "none";
   } else {
     repetition.checked = !repetitionPattern.test(pwdValue);
   }
@@ -49,27 +154,44 @@ function validatePassword() {
     repetition.checked &&
     usingnames.checked
   ) {
-    pwd.setCustomValidity("");
     if (pwdValue.length >= 12) {
       pwdMeter.value = 3;
-      pwdLabel.textContent = "Strong";
+      pwdMeterLabel.textContent = "Strong";
+      pwdLabel.style.display = "block";
+      pwdErrorMsg.textContent = "";
     } else {
       pwdMeter.value = 2;
-      pwdLabel.textContent = "Medium";
+      pwdMeterLabel.textContent = "Medium";
+      pwdLabel.style.display = "block";
+      pwdErrorMsg.textContent = "";
     }
   } else {
     pwdErrorMsg.textContent = "Please enter a valid password";
     if (pwdValue.length > 0) {
       pwdMeter.value = 1;
-      pwdLabel.textContent = "Weak";
+      pwdMeterLabel.textContent = "Weak";
     } else {
       pwdMeter.value = 0;
-      pwdLabel.textContent = "";
+      pwdMeterLabel.textContent = "";
     }
   }
 }
 
 pwd.addEventListener("input", validatePassword);
+
+pwd.addEventListener("blur", () => {
+  if (pwd.value === "") {
+    pwdErrorMsg.classList.add("unfocus");
+  }
+  validatePassword();
+});
+
+pwd.addEventListener("focus", () => {
+  pwdLabel.classList.add("labelUp");
+  if (pwdErrorMsg.classList.contains("unfocus")) {
+    pwdErrorMsg.classList.remove("unfocus");
+  }
+});
 
 // helper function
 
